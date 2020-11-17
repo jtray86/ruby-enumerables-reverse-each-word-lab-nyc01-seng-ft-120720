c2399b3df48ce5c0ca1ff220baf8bdb1b7f3018c
Jen Tracy <jenniferetracy@gmail.com>
2020-11-17 15:15:18 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,16 @@
+def reverse_each_word(string)
+  
+  sentence_array = string.split
+  
+  sentence_array.each { |reverse_word| reverse_word.reverse!}
+  
+  sentence_array.join(" ")
+
+end
+  
+ def reverse_each_word(string)
+   sentence_array = string.split
+   sentence_array.collect { |reverse_word| reverse_word.reverse!}
+   sentence_array.join(" ")
+   
+ end
